57298b23453f57f95102051ad6ea1febb1e7e01b
Ken Ho <ken@Kens-MBP.home>
2020-05-07 15:03:19 -0400

    collection-item

--- src/App.js
+++ src/App.js
@@ -2,16 +2,8 @@ import React from 'react';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-      
-      </div>
-      )
-    };
     
     // <button onClick={() => props.history.push('/hats/abc')}>history.push</button>
 
@@ -25,7 +17,7 @@ const App = (props) => {
 
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
 
       </Switch>
 
